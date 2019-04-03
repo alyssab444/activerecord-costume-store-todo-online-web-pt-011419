c17ae6080bc024a5dca18e9486589720f0138fa3
Alyssa Brown <alyssa.brown@live.com>
2019-04-03 02:54:14 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,7 +5,8 @@ class CreateCostumeStores < ActiveRecord::Migration
     add_column :costume, :location, :string
     add_column :costume, :costume_inventory, :integer
     add_column :costume, :num_of_empoloyees, :integer
-    add_column :costume, :
+    add_column :costume, :still_in_business, :float 
+    add_column :costume, :opening_time
     
     
   end 

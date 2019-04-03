65b40f4e605d1023bb6aa919d9f5df931bb24eac
Alyssa Brown <alyssa.brown@live.com>
2019-04-03 03:09:49 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,7 +1,12 @@
 class CreateHauntedHouses < ActiveRecord::Migration[4.1]
   
   def change
-    create_table 
+    create_table :haunted_houses do |t|
+      t.string :name 
+      t.string :location
+      t.string :theme
+      t.integer :price
+      t.
   
   
 end 

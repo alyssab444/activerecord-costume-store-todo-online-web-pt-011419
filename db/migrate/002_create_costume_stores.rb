1af23662505ebf6fcf216c2d7acafd4fbf5a1f30
Alyssa Brown <alyssa.brown@live.com>
2019-04-03 02:52:57 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -2,7 +2,10 @@ class CreateCostumeStores < ActiveRecord::Migration
   
   def change 
     add_column :costume, :name, :string 
-    add_column :costume, :location, :
+    add_column :costume, :location, :string
+    add_column :costume, :costume_inventory, :integer
+    add_column :costume, :number_of_empoloyees, :integer
+    
     
   end 
   

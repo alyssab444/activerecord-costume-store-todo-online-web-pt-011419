de6bff9312341d5ba489f61223adf65a0e4cbf6e
Alyssa Brown <alyssa.brown@live.com>
2019-04-03 02:23:55 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,8 +1,13 @@
 class CreateCostumes< ActiveRecord::Migration
 
-# By convention, the class name should match the part of the
-# file name after the number, so in this case:
-# 002_create_costumes.rb becomes class CreateCostumes
+  def change 
+    create_columns 
+    
+  end 
+  
+  
+  
+end 
 
 # Define a change method in which to do the migration
 # In this change method, create columns with the correct names and 
